8c4b51979a2f040ac1c4746b4bdc38a2e4ae5ff8
Robin Ochoa <robin.ochoa@rokk3rlabs.com>
2020-04-11 15:40:50 -0500

    Dummy Users

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,14 @@
 const graphql = require('graphql');
 
+// dummy data
+var userData = [
+    {id: '1', name: 'Bod', age: 36},
+    {id: '13', name: 'Anna', age: 26},
+    {id: '211', name: 'Bella', age: 16},
+    {id: '19', name: 'Gina', age: 26},
+    {id: '150', name: 'Georgina', age: 36},
+];
+
 const {
     GraphQLObjectType,
     GraphQLID,
@@ -30,6 +39,13 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: {id: {type: GraphQLString}},
             resolve(parent, args){
+                let user = {
+                    id: '345',
+                    age: 34,
+                    name: 'Paulo'
+                }
+
+                return user;
                 // we resolve with data
                 // get and return data from a datasource
             }
