91b71404847714da7d934af4bf6b4487aad004e6
Robin Ochoa <robin.ochoa@rokk3rlabs.com>
2020-04-14 16:18:43 -0500

    Hobby added

--- 00_Testing.md
+++ 00_Testing.md
@@ -1,5 +1,6 @@
 # Testing
 ```sh
+source ~/.bash_profile
 nodemon app
 
 # Browser
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,11 +3,19 @@ var _ = require('lodash');
 
 // dummy data
 var usersData = [
-    {id: '1', name: 'Bod', age: 36},
-    {id: '13', name: 'Anna', age: 26},
-    {id: '211', name: 'Bella', age: 16},
-    {id: '19', name: 'Gina', age: 26},
-    {id: '150', name: 'Georgina', age: 36}
+    {id: '1', name: 'Bod', age: 36, profession: 'Programmer'},
+    {id: '13', name: 'Anna', age: 26, profession: 'Baker'},
+    {id: '211', name: 'Bella', age: 16, profession: 'Mechanic'},
+    {id: '19', name: 'Gina', age: 26, profession: 'Painter'},
+    {id: '150', name: 'Georgina', age: 36, profession: 'Teacher'}
+];
+
+var hobbiesData = [
+    {id: '1', title: 'Programming', description: 'Using computers...'},
+    {id: '2', title: 'Rowing', description: 'Sweart and fell...'},
+    {id: '3', title: 'Swimming', description: 'Get in water...'},
+    {id: '4', title: 'Fencing', description: 'Hobby for fency...'},
+    {id: '5', title: 'Hiking', description: 'Wear hiking boots...'},
 ];
 
 const {
@@ -24,9 +32,20 @@ const UserType = new GraphQLObjectType({
     name: 'User',
     description: 'Documentation for user',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         age: {type: GraphQLInt},
+        profession: {type: GraphQLString}
+    })
+})
+
+const HobbyType = new GraphQLObjectType({
+    name: 'Hobby',
+    description: 'Documentation for hobby',
+    fields: () => ({
+        id: {type: GraphQLID},
+        title: {type: GraphQLString},
+        description: {type: GraphQLString}
     })
 })
 
@@ -43,6 +62,15 @@ const RootQuery = new GraphQLObjectType({
                 return _.find(usersData, {id: args.id})
                 // we resolve with data
                 // get and return data from a datasource
+            }
+        },
+
+        hobby: {
+            type: HobbyType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args){
+                // return data for our hobby
+                return _.find(hobbiesData, {id: args.id})
             }
         }
     })
